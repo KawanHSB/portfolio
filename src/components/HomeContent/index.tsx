63e58843a20b530ab6109fe1d036cb6d6cc28234
KawanHSB <kawanbatista@hotmail.com>
2022-11-25 20:07:50 -0300

    font import

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,19 +1,27 @@
 import styles from "./styles.module.scss"
 import "../../assets/kawan.png"
+import {  } from "react-icons"
 
 export function HomeContent() {
     return (
-        <div className={styles.wraper}>
-            <span className={styles.image}/>
-            <p>
-                Sup I'm Kawan a begginer at front-end development 
-                looking for a start at this market i've studied the 
-                basics of development at Alura and now i'm learning new 
-                tecnologies at RocketSeat check the techs for more,
-                 i've knowledge at agile methods as Scrum and XP, 
-                 never work at a project with XP but know some concepts
-            </p>
+        <div>
+            <div className={styles.wraper}>
+                <span className={styles.image}/>
+                <p>
+                    Sup I'm Kawan a begginer at front-end development 
+                    looking for a start at this market i've studied the 
+                    basics of development at Alura and now i'm learning new 
+                    tecnologies at RocketSeat check the techs for more,
+                    i've knowledge at agile methods as Scrum and XP, 
+                    never work at a project with XP but know some concepts
+                </p>
+            </div>
+            <div>
+                <a href="https://github.com/KawanHSB"></a>
+                <a href=""></a>
+            </div>
         </div>
+
     )
 }
 

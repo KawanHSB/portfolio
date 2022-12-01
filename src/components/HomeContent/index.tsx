7ec0dc9b26bea3d17893c8719e8d8d09b8746844
KawanHSB <kawanbatista@hotmail.com>
2022-12-01 07:11:07 -0300

    Main done

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,27 +1,27 @@
 import styles from "./styles.module.scss"
-import "../../assets/kawan.png"
-import {  } from "react-icons"
+import kawanAvatar from "../../assets/kawan.png"
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+
 
 export function HomeContent() {
     return (
         <div>
             <div className={styles.wraper}>
-                <span className={styles.image}/>
+                <img src={kawanAvatar} alt="" className={styles.elipse} />
                 <p>
-                    Sup I'm Kawan a begginer at front-end development 
-                    looking for a start at this market i've studied the 
-                    basics of development at Alura and now i'm learning new 
+                    Sup I'm Kawan a begginer at front-end development
+                    looking for a start at this market i've studied the
+                    basics of development at Alura and now i'm learning new
                     tecnologies at RocketSeat check the techs for more,
-                    i've knowledge at agile methods as Scrum and XP, 
+                    i've knowledge at agile methods as Scrum and XP,
                     never work at a project with XP but know some concepts
                 </p>
             </div>
-            <div>
-                <a href="https://github.com/KawanHSB"></a>
-                <a href=""></a>
-            </div>
+            <footer>
+                <a href="https://github.com/KawanHSB"><FaGithubSquare size={50} /></a>
+                <a href="https://www.linkedin.com/in/kawan-batista-221a67203/"><FaLinkedin size={50} /></a>
+            </footer>
         </div>
-
     )
 }
 
